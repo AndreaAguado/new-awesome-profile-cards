f48fac8baad22b51bf9ee76b755ca1fa07aaf413
CriSFV <cristina.f.valverde@gmail.com>
2021-09-27 13:42:34 +0200

    add Fill component

---
+++ src/components/Fill.js
@@ -0,0 +1,183 @@
+import { useState } from 'react';
+import '../styles/layout/Fill.scss';
+const Fill = () => {
+  const [stateDesign, setStateDesign] = useState('hidden');
+  const [stateFill, setStateFill] = useState('hidden');
+  const [stateShare, setStateShare] = useState('hidden');
+
+  const [arrowDesign, setArrowDesign] = useState('');
+  const [arrowFill, setArrowFill] = useState('');
+  const [arrowShare, setArrowShare] = useState('');
+  const [data, setData] = useState({
+    palette: 1,
+    name: '',
+    job: '',
+    phone: '',
+    email: '',
+    linkedin: '',
+    github: '',
+  });
+  const handleCollapsable = (ev) => {
+    const selected = ev.currentTarget;
+    console.log(selected);
+    if (selected.id === 'collapseDesign') {
+      setStateDesign('');
+      setStateFill('hidden');
+      setStateShare('hidden');
+      setArrowDesign('rotateArrowUp');
+      setArrowFill('');
+      setArrowShare('');
+    } else if (selected.id === 'collapseFill') {
+      setStateDesign('hidden');
+      setStateFill('');
+      setStateShare('hidden');
+      setArrowDesign('');
+      setArrowFill('rotateArrowUp');
+      setArrowShare('');
+    } else if (selected.id === 'collapseShare') {
+      setStateDesign('hidden');
+      setStateFill('hidden');
+      setStateShare('');
+      setArrowDesign('');
+      setArrowFill('');
+      setArrowShare('rotateArrowUp');
+    }
+  };
+  const handleInput = (ev) => {
+    const whichInput = ev.currentTarget.name;
+    if (whichInput === 'name') {
+      setData({ ...data, name: ev.currentTarget.value });
+    } else if (whichInput === 'job') {
+      setData({ ...data, job: ev.currentTarget.value });
+    } else if (whichInput === 'phone') {
+      setData({ ...data, phone: ev.currentTarget.value });
+    } else if (whichInput === 'email') {
+      setData({ ...data, email: ev.currentTarget.value });
+    } else if (whichInput === 'linkedin') {
+      setData({ ...data, linkedin: ev.currentTarget.value });
+    } else if (whichInput === 'github') {
+      setData({ ...data, github: ev.currentTarget.value });
+    }
+  };
+  return (
+    <fieldset className='fill'>
+      <section
+        onClick={handleCollapsable}
+        id='collapseFill'
+        className='section--title'
+      >
+        <i className='far fa-keyboard icon'></i>
+        <h3 className='title '>Rellena</h3>
+        <i className={`fas fa-chevron-down arrow ${arrowFill}`}></i>
+      </section>
+
+      <section className={stateFill}>
+        <div className='fill__data'>
+          <label htmlFor='name' className='fill__label'>
+            Nombre completo
+            <span className='fill__label--asterisk'>*</span>
+          </label>
+          <input
+            id='name'
+            placeholder='Ej: Groot!'
+            type='text'
+            name='name'
+            className='fill__input'
+            required=''
+            onChange={handleInput}
+            value={data.name}
+          />
+          <label htmlFor='job' className='fill__label'>
+            Puesto<span className='fill__label--asterisk'>*</span>
+          </label>
+
+          <input
+            id='job'
+            placeholder='Ej: Groot-developer'
+            type='text'
+            name='job'
+            className='fill__input'
+            required=''
+            onChange={handleInput}
+            value={data.job}
+          />
+        </div>
+
+        <div className='fill__photo'>
+          <label htmlFor='photo' className='fill__label'>
+            Imagen de perfil
+            <span className='fill__label--asterisk'>*</span>
+          </label>
+
+          <label className='fill__photo--upload-btn' htmlFor='img-selector'>
+            Añadir imagen
+          </label>
+          <input
+            type='file'
+            name='photo'
+            id='img-selector'
+            className='fill__photo--hiddenField'
+          />
+
+          <div className='fill__photo--preview'></div>
+        </div>
+
+        <div className='fill__media'>
+          <label htmlFor='email' className='fill__label'>
+            Email<span className='fill__label--asterisk'>*</span>
+          </label>
+          <input
+            id='email'
+            placeholder='Ej: iamgroot@gmail.com'
+            type='email'
+            name='email'
+            className='fill__input'
+            required=''
+            onChange={handleInput}
+            value={data.email}
+          />
+          <label htmlFor='phone' className='fill__label'>
+            Teléfono<span className='fill__label--asterisk'>*</span>
+          </label>
+          <input
+            id='phone'
+            placeholder='Ej: 555-55-55-55'
+            type='tel'
+            name='phone'
+            className='fill__input'
+            required=''
+            onChange={handleInput}
+            value={data.phone}
+          />
+          <label htmlFor='linkedin' className='fill__label'>
+            Linkedin<span className='fill__label--asterisk'>*</span>
+          </label>
+          <input
+            id='linkedin'
+            placeholder='Ej: linkedin.com/Groot'
+            type='text'
+            name='linkedin'
+            className='fill__input'
+            required=''
+            onChange={handleInput}
+            value={data.linkedin}
+          />
+          <label htmlFor='github' className='fill__label'>
+            Github<span className='fill__label--asterisk'>*</span>
+          </label>
+          <input
+            id='github'
+            placeholder='Ej: @Groot!'
+            type='text'
+            name='github'
+            className='fill__input'
+            required=''
+            onChange={handleInput}
+            value={data.github}
+          />
+        </div>
+      </section>
+    </fieldset>
+  );
+};
+export default Fill;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import '../styles/pages/Form.scss';
 import { useState } from 'react';
 import Design from './Design';
+import Fill from './Fill';
 const Form = () => {
   const [stateDesign, setStateDesign] = useState('hidden');
   const [stateFill, setStateFill] = useState('hidden');
@@ -44,144 +45,27 @@ const Form = () => {
       setArrowShare('rotateArrowUp');
     }
   };
-  const handleInput = (ev) => {
-    const whichInput = ev.currentTarget.name;
-    if (whichInput === 'name') {
-      setData({ ...data, name: ev.currentTarget.value });
-    } else if (whichInput === 'job') {
-      setData({ ...data, job: ev.currentTarget.value });
-    } else if (whichInput === 'phone') {
-      setData({ ...data, phone: ev.currentTarget.value });
-    } else if (whichInput === 'email') {
-      setData({ ...data, email: ev.currentTarget.value });
-    } else if (whichInput === 'linkedin') {
-      setData({ ...data, linkedin: ev.currentTarget.value });
-    } else if (whichInput === 'github') {
-      setData({ ...data, github: ev.currentTarget.value });
-    }
-  };
+  //   const handleInput = (ev) => {
+  //     const whichInput = ev.currentTarget.name;
+  //     if (whichInput === 'name') {
+  //       setData({ ...data, name: ev.currentTarget.value });
+  //     } else if (whichInput === 'job') {
+  //       setData({ ...data, job: ev.currentTarget.value });
+  //     } else if (whichInput === 'phone') {
+  //       setData({ ...data, phone: ev.currentTarget.value });
+  //     } else if (whichInput === 'email') {
+  //       setData({ ...data, email: ev.currentTarget.value });
+  //     } else if (whichInput === 'linkedin') {
+  //       setData({ ...data, linkedin: ev.currentTarget.value });
+  //     } else if (whichInput === 'github') {
+  //       setData({ ...data, github: ev.currentTarget.value });
+  //     }
+  //   };
   return (
     <form className='form'>
       <section className='select'>
         <Design />
-        <fieldset className='fill'>
-          <section
-            onClick={handleCollapsable}
-            id='collapseFill'
-            className='section--title'
-          >
-            <i className='far fa-keyboard icon'></i>
-            <h3 className='title '>Rellena</h3>
-            <i className={`fas fa-chevron-down arrow ${arrowFill}`}></i>
-          </section>
-
-          <section className={stateFill}>
-            <div className='fill__data'>
-              <label htmlFor='name' className='fill__label'>
-                Nombre completo
-                <span className='fill__label--asterisk'>*</span>
-              </label>
-              <input
-                id='name'
-                placeholder='Ej: Groot!'
-                type='text'
-                name='name'
-                className='fill__input'
-                required=''
-                onChange={handleInput}
-                value={data.name}
-              />
-              <label htmlFor='job' className='fill__label'>
-                Puesto<span className='fill__label--asterisk'>*</span>
-              </label>
-
-              <input
-                id='job'
-                placeholder='Ej: Groot-developer'
-                type='text'
-                name='job'
-                className='fill__input'
-                required=''
-                onChange={handleInput}
-                value={data.job}
-              />
-            </div>
-
-            <div className='fill__photo'>
-              <label htmlFor='photo' className='fill__label'>
-                Imagen de perfil
-                <span className='fill__label--asterisk'>*</span>
-              </label>
-
-              <label className='fill__photo--upload-btn' htmlFor='img-selector'>
-                Añadir imagen
-              </label>
-              <input
-                type='file'
-                name='photo'
-                id='img-selector'
-                className='fill__photo--hiddenField'
-              />
-
-              <div className='fill__photo--preview'></div>
-            </div>
-
-            <div className='fill__media'>
-              <label htmlFor='email' className='fill__label'>
-                Email<span className='fill__label--asterisk'>*</span>
-              </label>
-              <input
-                id='email'
-                placeholder='Ej: iamgroot@gmail.com'
-                type='email'
-                name='email'
-                className='fill__input'
-                required=''
-                onChange={handleInput}
-                value={data.email}
-              />
-              <label htmlFor='phone' className='fill__label'>
-                Teléfono<span className='fill__label--asterisk'>*</span>
-              </label>
-              <input
-                id='phone'
-                placeholder='Ej: 555-55-55-55'
-                type='tel'
-                name='phone'
-                className='fill__input'
-                required=''
-                onChange={handleInput}
-                value={data.phone}
-              />
-              <label htmlFor='linkedin' className='fill__label'>
-                Linkedin<span className='fill__label--asterisk'>*</span>
-              </label>
-              <input
-                id='linkedin'
-                placeholder='Ej: linkedin.com/Groot'
-                type='text'
-                name='linkedin'
-                className='fill__input'
-                required=''
-                onChange={handleInput}
-                value={data.linkedin}
-              />
-              <label htmlFor='github' className='fill__label'>
-                Github<span className='fill__label--asterisk'>*</span>
-              </label>
-              <input
-                id='github'
-                placeholder='Ej: @Groot!'
-                type='text'
-                name='github'
-                className='fill__input'
-                required=''
-                onChange={handleInput}
-                value={data.github}
-              />
-            </div>
-          </section>
-        </fieldset>
+        <Fill />
 
         <fieldset className='share'>
           <section
